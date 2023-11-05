94ad6c50d0b2b6cc529d64dee20d6eda85a3d0dc
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-11-06 01:56:07 +0600

    dotenv file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port =process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -10,10 +11,10 @@ app.use(express.json());
 
 // sanjidamitu456
 // yqXSY9g3I7IkGS9o
+console.log(process.env.DB_USER)
 
 
-
-const uri = "mongodb+srv://sanjidamitu456:yqXSY9g3I7IkGS9o@cluster0.5du28se.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5du28se.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
